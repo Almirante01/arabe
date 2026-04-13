fb92a1f05daaaa0f1865c1571dc46f7d279ffbe8
Almirante01 <almirante01@hotmail.com>
2026-04-14 01:37:40 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,36 @@
-alert("JS FUNCIONANDO");
+let words = [
+ {ar:"كَتَبَ", es:"escribir", type:"verbo"},
+ {ar:"مُدَرِّسٌ", es:"profesor", type:"sustantivo"},
+ {ar:"بَيْتٌ", es:"casa", type:"lugar"},
+ {ar:"قِطٌّ", es:"gato", type:"animal"}
+];
+
+// 🔥 HARAKAT COLOREADOS (VERSIÓN QUE FUNCIONA)
+function highlight(text){
+  return text
+    .replace(/َ/g, '<span style="color:blue">َ</span>')
+    .replace(/ِ/g, '<span style="color:green">ِ</span>')
+    .replace(/ُ/g, '<span style="color:red">ُ</span>')
+    .replace(/ْ/g, '<span style="color:gray">ْ</span>')
+    .replace(/ّ/g, '<span style="color:purple">ّ</span>');
+}
+
+// MOSTRAR
+function display(){
+  let list = document.getElementById("list");
+
+  let html = "";
+
+  words.forEach(w=>{
+    html += `
+      <div class="card">
+        <div class="arabe">${highlight(w.ar)}</div>
+        <div>${w.es}</div>
+      </div>
+    `;
+  });
+
+  list.innerHTML = html;
+}
+
+display();
