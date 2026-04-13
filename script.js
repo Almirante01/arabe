09e0e2f7c9dafaa130f733db9e714bb3d6f2ddfc
Almirante01 <almirante01@hotmail.com>
2026-04-14 01:31:48 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,17 +1,1 @@
-function renderArabic() {
-
-  let html = `
-  <div style="font-size:60px; direction:rtl; display:flex; gap:5px; justify-content:center;">
-    
-    <div>م<span style="color:red">ُ</span></div>
-    <div>د<span style="color:blue">َ</span></div>
-    <div>ر<span style="color:green">ِ</span><span style="color:purple">ّ</span></div>
-    <div>س<span style="color:gray">ْ</span></div>
-  
-  </div>
-  `;
-
-  document.body.innerHTML = html;
-}
-
-renderArabic();
+alert("JS FUNCIONANDO");
